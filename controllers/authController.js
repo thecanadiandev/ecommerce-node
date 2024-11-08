b34e7743b154a0b040acb9a1e98c865d97c0116c
Hari <harikrishkk@gmail.com>
2024-11-08 14:39:00 -0500

    add postman collection

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
 const register = async (req, res) => {
   console.log("register user")
+  res.send("register user")
 }
 
 const login = async (req, res) => {
   console.log("login user")
+  res.send("login user")
 }
 
 const logout = async (req, res) => {
   console.log("logout user")
+  res.send("logout user")
 } 
 
 module.exports = {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { register, login, logout } = require('../controllers/authController');
 
+// ADD POSTMAN COLLECTION FOR ALL THESE 
+
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);

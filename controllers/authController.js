14cf581e86dc4a0a0284a4938ba69deed81a4e48
Hari <harikrishkk@gmail.com>
2024-11-08 15:23:01 -0500

    JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const { email , name, password} = req.body;
@@ -13,7 +14,9 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  res.status(StatusCodes.CREATED).json({user})
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
+  res.status(StatusCodes.CREATED).json({user: tokenUser, token })
 }
 
 const login = async (req, res) => {

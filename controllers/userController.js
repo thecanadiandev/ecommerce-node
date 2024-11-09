2d5782342dfa762bb3f9573177488e537e8c8ace
Hari <harikrishkk@gmail.com>
2024-11-09 09:57:25 -0500

    authenticate user complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 const getAllUsers = async (req, res) => {
+  // req.user exists because of authenticateUser middleware
   const users = await User.find({ role: 'user' }).select('-password');
   res.status(StatusCodes.OK).json({ users });
 };
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,19 @@ const authenticateUser = async (req, res, next) => {
   if (!token) {
     throw new CustomError.UnauthenticatedError('Authentication invalid');
   }
-  next();
+  try {
+    const payload = isTokenValid({ token });
+    // console.log(payload);
+    // Attach the user and his permissions to the req object
+    req.user = {
+      userId: payload.user.userId,
+      role: payload.user.role,
+      name: payload.user.name
+    };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication invalid');
+  }
 }
 
 module.exports = { authenticateUser };

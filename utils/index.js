ded95ce60540b78c20ea6569d05beeeaddd90e3a
Hari <harikrishkk@gmail.com>
2024-11-11 13:44:06 -0500

    createToken user function

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,3 @@
 const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
-module.exports = { createJWT, isTokenValid, attachCookiesToResponse };
+const { createTokenUser } = require('./createTokenUser');
+module.exports = { createJWT, isTokenValid, attachCookiesToResponse, createTokenUser };

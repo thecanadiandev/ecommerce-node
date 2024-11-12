fb5cdec62ff2de08144f24eed9ff5277e71a90b0
Hari <harikrishkk@gmail.com>
2024-11-12 10:19:59 -0500

    add product routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { getAllProducts, getSingleProduct, createProduct, updateProduct, deleteProduct, uploadImage } = require('../controllers/productController');
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
+
+
+router.route('/')
+  .get(getAllProducts)
+  .post(authenticateUser, authorizePermissions('admin'), createProduct);
+
+router.route('/uploadImage')
+  .post(authenticateUser, authorizePermissions('admin'), uploadImage);
+
+router.route('/:id')
+  .get(getSingleProduct)
+  .patch(authenticateUser, authorizePermissions('admin'), updateProduct)
+  .delete(authenticateUser, authorizePermissions('admin'), deleteProduct)
+  
+module.exports = router;

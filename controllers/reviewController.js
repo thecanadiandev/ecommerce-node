cbb6fee4d317153d5cc8a3b4a696d19ec6fbb6b8
Hari <harikrishkk@gmail.com>
2024-11-12 17:53:30 -0500

    populate

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -21,7 +21,9 @@ const createReview = async (req, res) => {
 }
 
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({})
+    .populate({ path: 'product', select: 'name company price' })
+    .populate({ path: 'user', select: 'name' })
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 }
 

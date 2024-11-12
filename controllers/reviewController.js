2cb63d7dbd96a907b3aa43c6247d94cd33944637
Hari <harikrishkk@gmail.com>
2024-11-12 17:42:31 -0500

    delete review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -39,7 +39,15 @@ const updateReview = async (req, res) => {
 } 
 
 const deleteReview = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Delete review' });
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {    
+     throw new CustomError.NotFoundError(`No review with id, ${reviewId}`);
+  }
+  checkPermissions(req.user, review.user);
+  await review.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success, review removed' });
 } 
 
 module.exports = {

64a02f47b6c60e3a39c7fa363e7c32ed2f5f4ad5
Hari <harikrishkk@gmail.com>
2024-11-12 22:42:09 -0500

    create order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,9 +6,67 @@ const CustomError = require('../errors');
 const { checkPermissions } = require('../utils');
 
 
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = 'someRandomValue';
+  return { client_secret, amount };
+};
+
 const createOrder = async (req, res) => {
-  console.log("createOrder")
-  res.status(StatusCodes.OK).json({ message: 'create order' });
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError('No cart items provided');
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      'Please provide tax and shipping fee'
+    );
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with id : ${item.product}`
+      );
+    }
+    const { name, price, image, _id } = dbProduct;
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // add item to order
+    orderItems = [...orderItems, singleOrderItem];
+    // calculate subtotal
+    subtotal += item.amount * price;
+  }
+  // calculate total
+  const total = tax + shippingFee + subtotal;
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: 'usd',
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 const getAllOrders = async (req, res) => {
   console.log("getAllOrders")

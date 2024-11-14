46b2460d9119309263d990fbbd1dfa3543bd7227
Hari <harikrishkk@gmail.com>
2024-11-13 19:44:11 -0500

    security packages

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 // DB 
 const connectDB = require('./db/connect');
@@ -22,6 +27,18 @@ const orderRouter = require('./routes/orderRoutes')
 // middleware 
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // limit each IP to 60 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(cors());
 
 app.use(morgan('tiny'));
 app.use(express.json());
